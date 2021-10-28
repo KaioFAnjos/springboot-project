2ecf80d5b8eaa69f2db3b6fff748ad1453a0a656
KaioFAnjos <kaio.fa@hotmail.com>
2021-10-28 00:24:30 -0300

    Service layer, component registration

--- src/main/java/com/kaiofanjos/projeto_spring/repositories/UserRepository.java
+++ src/main/java/com/kaiofanjos/projeto_spring/repositories/UserRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kaiofanjos.projeto_spring.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-	
-	
+		
 }
