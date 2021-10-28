05b1c5ede62a89a728f0c6f551eda197e8770881
KaioFAnjos <kaio.fa@hotmail.com>
2021-10-27 23:05:19 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/kaiofanjos/projeto_spring/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.kaiofanjos.projeto_spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kaiofanjos.projeto_spring.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+}
